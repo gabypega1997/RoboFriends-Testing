00c0e233db73d1a2a9d970cb98946151babc0e32
gabypega1997 <gabypega1997@gmail.com>
2022-03-31 10:47:31 +0200

    upload root app

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore,applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware  from 'redux-thunk';
+import './index.css';
+import App from './containers/App';
+
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import 'tachyons'; 
+import { searchRobots , requestRobots} from './reducer';
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById('root'));
+
+serviceWorkerRegistration.register();
+
