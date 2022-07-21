7608439a53a768d364fa0722f560b98743396621
gabypega1997 <gabypega1997@gmail.com>
2022-07-21 12:49:01 +0200

    prettier precommit try

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,13 +1,11 @@
 import React from "react";
 import CounterButton from "./CounterButton";
-import CounterButton2 from "./CounterButton2";
 
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className="pa2">
       <div className="ma3 pa2">
         <CounterButton className="tc"></CounterButton>
-        <CounterButton2 className="tc"></CounterButton2>
       </div>
       <input
         aria-label="Search Robots"
